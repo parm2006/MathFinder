9be2c318e0ec35872d52e59729d9afa939fed1a9
Parth Mohnot <150487691+parm2006@users.noreply.github.com>
2026-08-14 13:22:53 -0700

    bruteforce smarter bfs

--- bruteforce.py
+++ bruteforce.py
@@ -1,15 +1,16 @@
-from math import sqrt
-from math import e as E
+from collections import deque
+from math import sqrt, e as E
 operations = ['+', '-', '*', '/'] 
 nums = [0,1,2,3,4,5,6,7,8,9,10]
 
 found = False
 def solve(target):
-    for i in range(1,6):
-        do(i,target,f"{i}",depth=0)
+    dobfs(target)
+    # for i in range(1,6):
+    #     dobfs(i,target,f"{i}",depth=0)
 
 
-def do(inp,tar,opstr,depth=0):
+def dodfs(inp,tar,opstr,depth=0):
     global found
     if depth == 5:
         return
@@ -21,13 +22,43 @@ def do(inp,tar,opstr,depth=0):
         return
     
     if inp < 22:
-        do(E**inp,tar,f"(e^({opstr}))",depth+1)
+        dodfs(E**inp,tar,f"(e^({opstr}))",depth+1)
     if inp>0:
-        do(sqrt(inp),tar,f"sqrt({opstr})",depth+1)
+        dodfs(sqrt(inp),tar,f"sqrt({opstr})",depth+1)
     for i in nums:
-        do(inp+i,tar,f"({opstr}+{i})",depth+1)
-        do(inp-i,tar,f"({opstr}-{i})",depth+1)
-        do(inp*i,tar,f"({opstr}*{i})",depth+1)
+        dodfs(inp+i,tar,f"({opstr}+{i})",depth+1)
+        dodfs(inp-i,tar,f"({opstr}-{i})",depth+1)
+        dodfs(inp*i,tar,f"({opstr}*{i})",depth+1)
         if i != 0:
-            do(inp/i,tar,f"({opstr}/{i})",depth+1)
+            dodfs(inp/i,tar,f"({opstr}/{i})",depth+1)
         
+
+def dobfs(target):
+    queue = deque((i,target, f"{i}", 0) for i in nums)
+    seen = set()
+    while len(queue) != 0:
+        inp,tar,opstr,depth = queue.popleft()
+        if inp in seen:
+            continue
+        else:
+            seen.add(inp)
+        if depth == 5:
+            continue
+        elif abs(inp - tar) < 0.0001:
+            print(opstr)
+            return
+        
+        if inp < 22:
+            queue.append((E**inp,tar,f"(e^({opstr}))",depth+1))
+        if inp>0:
+            queue.append((sqrt(inp),tar,f"sqrt({opstr})",depth+1))
+        
+        for i in nums:
+            queue.append((inp+i,tar,f"({opstr}+{i})",depth+1))
+            queue.append((inp-i,tar,f"({opstr}-{i})",depth+1))
+            queue.append((inp*i,tar,f"({opstr}*{i})",depth+1))
+            if i != 0:
+                queue.append((inp/i,tar,f"({opstr}/{i})",depth+1))
+    
+    print("Not Found")
+    return
--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 import bruteforce as bf
+import sys
+
 
 if __name__ == "__main__":
-    bf.solve()
+    if len(sys.argv) > 1:
+        try:
+            target = float(sys.argv[1])
+            bf.solve(target)
+        except ValueError:
+            print("Please provide a valid number")
+    else:
+        print("Please provide a number")
