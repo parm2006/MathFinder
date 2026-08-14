5b7558a169aa1ec3a73c814f7d175d7a27709157
Parth Mohnot <150487691+parm2006@users.noreply.github.com>
2026-08-14 13:04:00 -0700

    no intelligence dfs brute force

--- bruteforce.py
+++ bruteforce.py
@@ -1,30 +1,33 @@
-from bruteforce import found
 from math import sqrt
 from math import e as E
 operations = ['+', '-', '*', '/'] 
-nums = [1,2,3,4,5]
+nums = [0,1,2,3,4,5,6,7,8,9,10]
 
 found = False
 def solve(target):
     for i in range(1,6):
-        do(i,target,"i")
+        do(i,target,f"{i}",depth=0)
 
 
-def do(inp,tar,opstr):
-    print(opstr)
+def do(inp,tar,opstr,depth=0):
     global found
-
+    if depth == 5:
+        return
     if found == True:
         return
     elif inp == tar:
         found = True
         print(opstr)
         return
+    
+    if inp < 22:
+        do(E**inp,tar,f"(e^({opstr}))",depth+1)
+    if inp>0:
+        do(sqrt(inp),tar,f"sqrt({opstr})",depth+1)
     for i in nums:
-        do(inp+i,tar,opstr+"+i")
-        do(inp-i,tar,opstr+"-i")
-        do(inp*i,tar,opstr+"*i")
-        do(inp/i,tar,opstr+"/i")
-        do(E**inp,tar,"e^("+opstr+")")
-        if inp>0:
-            do(sqrt(inp),tar,"sqrt("+opstr+")")
+        do(inp+i,tar,f"({opstr}+{i})",depth+1)
+        do(inp-i,tar,f"({opstr}-{i})",depth+1)
+        do(inp*i,tar,f"({opstr}*{i})",depth+1)
+        if i != 0:
+            do(inp/i,tar,f"({opstr}/{i})",depth+1)
+        
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import bruteforce as bf
 
 if __name__ == "__main__":
-    bf.solve(1.1)
+    bf.solve()
